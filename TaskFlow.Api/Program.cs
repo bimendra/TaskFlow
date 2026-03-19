b929d198531d24d1fa07840c2df0b31258d5ae46
Bimendra Bandara <bimendra.c.bandara@gmail.com>
2026-03-20 02:33:22 +0800

    chore: initialise TaskFlow solution with ASP.NET Core Web API and React (Vite) frontend scaffold

--- TaskFlow.Api/Program.cs
+++ TaskFlow.Api/Program.cs
@@ -4,11 +4,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 
