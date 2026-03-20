c2e03ce01d6b0fb03f7046e9823aaa2bf35e205b
Bimendra Bandara <bimendra.c.bandara@gmail.com>
2026-03-20 20:31:08 +0800

    Update CORS policy URL for the client

--- TaskFlow.Api/Program.cs
+++ TaskFlow.Api/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:62680")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
